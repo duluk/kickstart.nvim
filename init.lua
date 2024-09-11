2a437e93c945216d1054ebe2839f31193855a78d
James Bruce <jab3@korazin.net>
2024-09-11 16:32:59 -0500

    Set have_nerd_font to true so web-devicons will work

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- Set one of the built-in colorschemes as default
 --vim.cmd.colorscheme 'koehler'
@@ -961,8 +961,6 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
-  require 'kickstart.plugins.web-dev-icons',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
