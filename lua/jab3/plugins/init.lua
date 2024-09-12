ed232c01db4082d254c730e59bfd67ad80aa158e
James Bruce <jab3@korazin.net>
2024-09-11 22:57:04 -0500

    Remove the ui icons block at end
    Another syntax issue; wouldn't load with this. Will have to figure this
    out.

--- lua/jab3/plugins/init.lua
+++ lua/jab3/plugins/init.lua
@@ -720,7 +720,7 @@ return {
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
-}, {
+}--[[, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
@@ -740,4 +740,4 @@ return {
       lazy = '💤 ',
     },
   },
-}
+} --]]
