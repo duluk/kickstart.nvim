daa858841ca3ca166e4aa23c4312a808c27f45f9
James Bruce <jab3@korazin.net>
2024-09-28 21:02:11 -0500

    Remove commented out code - it's in git now lol

--- lua/jab3/plugins/copilot.lua
+++ lua/jab3/plugins/copilot.lua
@@ -1,12 +1,3 @@
--- local function get_hostname()
---   local f = io.popen '/bin/hostname'
---   local hostname = f:read '*a' or ''
---   f:close()
---   hostname = string.gsub(hostname, '\n$', '')
---   return hostname
--- end
-
--- local hostname = get_hostname()
 local hostname = vim.fn.hostname()
 
 if hostname ~= '<redacted>' then
