d77d3aa4be94678f2a91a9a8480e94160b0ab4e5
James Bruce <jab3@korazin.net>
2024-10-05 13:58:10 -0400

    Make visual blocks work as God intended

--- lua/jab3/core/options.lua
+++ lua/jab3/core/options.lua
@@ -57,6 +57,9 @@ end)
 -- Enable break indent
 vim.opt.breakindent = true
 
+-- One of the greatest optinos ever
+vim.opt.virtualedit = 'block'
+
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
