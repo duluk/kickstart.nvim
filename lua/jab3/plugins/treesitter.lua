6a2e7a4c6fba9ff164d6f6a10772f2062799ed6f
James Bruce <jab3@korazin.net>
2024-10-05 16:42:35 -0400

    Keymap for fugitive and some comments

--- lua/jab3/plugins/treesitter.lua
+++ lua/jab3/plugins/treesitter.lua
@@ -46,10 +46,18 @@ return { -- Highlight, edit, and navigate code
           node_decremental = '<bs>', -- default, grm
         },
       },
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
+
+      -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
+
       modules = {},
+
+      -- List of parsers to ignore installing (or "all")
       ignore_install = {},
+
       highlight = {
         enable = true,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
