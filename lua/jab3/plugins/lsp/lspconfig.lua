483418965d198172339bc30a6b0815d656ae493c
James Bruce <jab3@korazin.net>
2024-10-07 00:16:05 -0500

    Don't try to install LSPs for Windows

--- lua/jab3/plugins/lsp/lspconfig.lua
+++ lua/jab3/plugins/lsp/lspconfig.lua
@@ -197,99 +197,105 @@ return {
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
-      local servers = {
-        clangd = {
-          keys = {
-            { '<leader>ch', '<cmd>ClangdSwitchSourceHeader<CR>', desc = 'Switch Source/Header (C/C++)' },
-          },
-          root_dir = function(fname)
-            return require('lspconfig.util').root_pattern(
-              'Makefile',
-              'configure.ac',
-              'configure.in',
-              'config.h.in',
-              'meson.build',
-              'meson_options.txt',
-              'build.ninja'
-            )(fname) or require('lspconfig.util').root_pattern('compile_commands.json', 'compile_flags.txt')(fname) or require('lspconfig.util').find_git_ancestor(
-              fname
-            )
-          end,
-          capabilities = {
-            -- So it plays well with GitHub Copilot
-            offsetEncoding = { 'utf-16' },
-          },
-          cmd = {
-            'clangd',
-            '--background-index',
-            '--clang-tidy',
-            '--header-insertion=iwyu',
-            '--completion-style=detailed',
-            '--function-arg-placeholders',
-            '--fallback-style=llvm',
-          },
-          init_options = {
-            usePlaceholders = true,
-            completeUnimported = true,
-            clangdFileStatus = true,
-          },
-        },
-        ['ast-grep'] = {}, -- Used to search for symbols
-        ['awk-language-server'] = {},
-        beautysh = {}, -- Used to format shell scripts
-        ['clang-format'] = {},
-        ['cmake-language-server'] = {},
-        cmakelang = {},
-        cmakelint = {},
-        cpplint = {},
-        ['css-lsp'] = {},
-        -- erb_lint = {},
-        eslint = {},
-        eslint_d = {}, -- This does use an underscore
-        flake8 = {}, -- Used to lint Python
-        ['golangci-lint-langserver'] = {},
-        ['html-lsp'] = {},
-        jsonlint = {},
-        ['lua-language-server'] = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+
+      -- Windows doesn't install most of these LSPs very well, or they're not supported at all so just ignore them
+      local os_name = vim.loop.os_uname().sysname
+      local servers = {}
+      if os_name ~= 'Windows_NT' then
+        servers = {
+          clangd = {
+            keys = {
+              { '<leader>ch', '<cmd>ClangdSwitchSourceHeader<CR>', desc = 'Switch Source/Header (C/C++)' },
+            },
+            root_dir = function(fname)
+              return require('lspconfig.util').root_pattern(
+                'Makefile',
+                'configure.ac',
+                'configure.in',
+                'config.h.in',
+                'meson.build',
+                'meson_options.txt',
+                'build.ninja'
+              )(fname) or require('lspconfig.util').root_pattern('compile_commands.json', 'compile_flags.txt')(fname) or require('lspconfig.util').find_git_ancestor(
+                fname
+              )
+            end,
+            capabilities = {
+              -- So it plays well with GitHub Copilot
+              offsetEncoding = { 'utf-16' },
+            },
+            cmd = {
+              'clangd',
+              '--background-index',
+              '--clang-tidy',
+              '--header-insertion=iwyu',
+              '--completion-style=detailed',
+              '--function-arg-placeholders',
+              '--fallback-style=llvm',
+            },
+            init_options = {
+              usePlaceholders = true,
+              completeUnimported = true,
+              clangdFileStatus = true,
             },
           },
-        },
-        -- lua_language_server = {}, -- LSP for Lua
-        ols = {}, -- LSP for Odin
-        prettier = {},
-        -- 'python-lsp-server = {}, -- LSP for Python
-        pyright = {}, -- LSP for Python (better allegedly, but by MS)
-        rubocop = {},
-        ['ruby-lsp'] = {},
-        rubyfmt = {},
-        ['rust-analyzer'] = {},
-        shellharden = {}, -- Used to lint shell scripts
-        stylua = {},
-        ['ts-standard'] = {},
-        ['typescript-language-server'] = {},
-        zls = {}, -- LSP for Zig
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-      }
+          ['ast-grep'] = {}, -- Used to search for symbols
+          ['awk-language-server'] = {},
+          beautysh = {}, -- Used to format shell scripts
+          ['clang-format'] = {},
+          ['cmake-language-server'] = {},
+          cmakelang = {},
+          cmakelint = {},
+          cpplint = {},
+          ['css-lsp'] = {},
+          -- erb_lint = {},
+          eslint = {},
+          eslint_d = {}, -- This does use an underscore
+          flake8 = {}, -- Used to lint Python
+          ['golangci-lint-langserver'] = {},
+          ['html-lsp'] = {},
+          jsonlint = {},
+          ['lua-language-server'] = {
+            -- cmd = {...},
+            -- filetypes = { ...},
+            -- capabilities = {},
+            settings = {
+              Lua = {
+                completion = {
+                  callSnippet = 'Replace',
+                },
+                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                -- diagnostics = { disable = { 'missing-fields' } },
+              },
+            },
+          },
+          -- lua_language_server = {}, -- LSP for Lua
+          ols = {}, -- LSP for Odin
+          prettier = {},
+          -- 'python-lsp-server = {}, -- LSP for Python
+          pyright = {}, -- LSP for Python (better allegedly, but by MS)
+          rubocop = {},
+          ['ruby-lsp'] = {},
+          rubyfmt = {},
+          ['rust-analyzer'] = {},
+          shellharden = {}, -- Used to lint shell scripts
+          stylua = {},
+          ['ts-standard'] = {},
+          ['typescript-language-server'] = {},
+          zls = {}, -- LSP for Zig
+          -- gopls = {},
+          -- pyright = {},
+          -- rust_analyzer = {},
+          -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
+          --
+          -- Some languages (like typescript) have entire language plugins that can be useful:
+          --    https://github.com/pmizio/typescript-tools.nvim
+          --
+          -- But for many setups, the LSP (`tsserver`) will work just fine
+          -- tsserver = {},
+          --
+        }
+      end
       -- setup = {
       --   clangd = function(_, opts)
       --     local clangd_ext_opts = LazyVim.opts("clangd_extensions.nvim")
