f0768ba7d22a8a5c476b5047ed96627245f129e6
Trey Bruce <trey.bruce@levelats.com>
2024-10-30 21:15:13 -0400

    Exclude ruby from treesitter, causes formatting issues

--- lua/jab3/plugins/treesitter.lua
+++ lua/jab3/plugins/treesitter.lua
@@ -64,11 +64,11 @@ return { -- Highlight, edit, and navigate code
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        -- additional_vim_regex_highlighting = { 'ruby' },
-        additional_vim_regex_highlighting = {},
+        additional_vim_regex_highlighting = { 'ruby' },
+        -- additional_vim_regex_highlighting = {},
       },
-      -- indent = { enable = true, disable = { 'ruby' } },
-      indent = { enable = true },
+      indent = { enable = true, disable = { 'ruby' } },
+      -- indent = { enable = true },
     }
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
