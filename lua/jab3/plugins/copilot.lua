77cc2d31808975b1d91146387f5826b82b34bb92
Trey Bruce <jab3@korazin.net>
2024-12-08 19:59:07 -0600

    add config for colorscheme for copilot suggestions

--- lua/jab3/plugins/copilot.lua
+++ lua/jab3/plugins/copilot.lua
@@ -1,5 +1,19 @@
 local hostname = vim.fn.hostname()
 
+vim.api.nvim_create_autocmd('ColorScheme', {
+  desc = 'Set the color used for Copilot suggestions, no matter the color scheme',
+  pattern = '*',
+  callback = function()
+    vim.api.nvim_set_hl(0, 'CopilotSuggestion', {
+      -- fg = '#555555', -- darker gray (too dark)
+      fg = '#ADD8E6', -- light blue
+      -- fg = '#6C757D', -- lighter gray
+      ctermfg = 8,
+      force = true,
+    })
+  end,
+})
+
 if hostname ~= '<redacted>' then
   return {
     {
