b1b9bca08b15a87a9eafbfa136639c1546b9b29f
Trey Bruce <jab3@korazin.net>
2025-02-26 13:40:42 -0600

    Remove zbirenbaum and use github copilot

--- lua/jab3/plugins/copilot.lua
+++ lua/jab3/plugins/copilot.lua
@@ -5,73 +5,37 @@ vim.api.nvim_create_user_command('CopilotToggle', function()
 end, {})
 vim.keymap.set('n', '<leader>ct', '<Cmd>CopilotToggle<CR>', { noremap = true, silent = true })
 
+vim.api.nvim_create_autocmd('ColorScheme', {
+  desc = 'Set the color used for Copilot suggestions, no matter the color scheme',
+  pattern = '*',
+  callback = function()
+    vim.api.nvim_set_hl(0, 'CopilotSuggestion', {
+      -- fg = '#555555', -- darker gray (too dark)
+      fg = '#ADD8E6', -- light blue
+      -- fg = '#6C757D', -- lighter gray
+      ctermfg = 8,
+      force = true,
+    })
+  end,
+})
+
 return {
   {
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    event = 'InsertEnter',
+    'github/copilot.vim',
     config = function()
-      require('copilot').setup {
-        -- Node.js version must be 16.x or higher
-        copilot_node_command = 'node',
-        -- Turn these off when using copilot_cmp:
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-        filetypes = {
-          bash = true,
-          c = true,
-          cpp = true,
-          go = true,
-          javascript = true,
-          json = true,
-          lua = true,
-          markdown = true,
-          python = true,
-          ruby = true,
-          rust = true,
-          sh = true,
-          toml = true,
-          typescript = true,
-          vim = true,
-          yaml = true,
-          zig = true,
-          zsh = true,
-          ['.'] = false,
-        },
+      vim.g.copilot_enabled = 1
+      vim.g.copilot_workspace_folders = { '~/src/mine' }
+      vim.g.copilot_filetypes = {
+        ['*'] = true,
+        ['dap-repl'] = false,
+        ['dapui_breakpoints'] = false,
+        ['dapui_console'] = false,
+        ['dapui_scopes'] = false,
+        ['dapui_stacks'] = false,
+        ['dapui_watches'] = false,
+        ['dapui_hover'] = false,
+        ['dapui_repl'] = false,
       }
     end,
   },
-  {
-    'zbirenbaum/copilot-cmp',
-    after = 'copilot.lua',
-    config = function()
-      require('copilot_cmp').setup()
-    end,
-  },
 }
-
--- vim.api.nvim_create_autocmd('ColorScheme', {
---   desc = 'Set the color used for Copilot suggestions, no matter the color scheme',
---   pattern = '*',
---   callback = function()
---     vim.api.nvim_set_hl(0, 'CopilotSuggestion', {
---       -- fg = '#555555', -- darker gray (too dark)
---       fg = '#ADD8E6', -- light blue
---       -- fg = '#6C757D', -- lighter gray
---       ctermfg = 8,
---       force = true,
---     })
---   end,
--- })
-
--- return {
---   {
---     'github/copilot.vim',
---     config = function()
---       vim.g.copilot_enabled = 1
---       vim.g.copilot_workspace_folders = { '~/src/mine' }
---       -- vim.g.copilot_no_tab_map = 1
---       -- vim.g.copilot_no_insert_completion = 1
---     end,
---   },
--- }
