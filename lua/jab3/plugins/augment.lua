de460731460214eb61f108b30760850a071bf243
Trey Bruce <jab3@korazin.net>
2025-02-26 14:14:56 -0600

    Disable Augment on startup

--- lua/jab3/plugins/augment.lua
+++ lua/jab3/plugins/augment.lua
@@ -2,6 +2,7 @@ return {
   {
     'augmentcode/augment.vim',
     config = function()
+      vim.g.augment_enabled = 0
       vim.g.augment_workspace_folders = {
         '~/src/open-webui',
         '~/src/mine',
